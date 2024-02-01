544ea8e7dcdc2402813437651f1c9cdbd5f9b63d
bleeth <xiao5406710@foxmail.com>
2024-02-01 22:05:14 +0800

    完善：NiceFlow sql命令

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 > 类似Kettle数据ETL工具，同时比Kettle更加易用和轻量
 
 ### 特性
+
 - 基于python的插件机制,目前提供70+插件,同时支持自定义插件
 - 基于json的flow任务，支持自定义任务配置
-
 
 ![img.png](doc/doc/img/demo.png)
 
@@ -15,25 +15,50 @@
 pip install NiceFlow
 ```
 
-
 #### 测试案例
 
 - plugin_test.py 测试插件功能
 - flow_test.py 测试flow功能
 
-
 #### cli使用
+
+##### 安装
 
 ```shell
 pip install NiceFlow
-
-# 执行flow任务
-NiceFlow exec --path csv_input_ck_output.json
-
-NiceFlow exec --path 1.json
-
 ```
 
+##### exec模式执行flow任务
+
+```shell
+NiceFlow exec --path csv_input_ck_output.json
+
+# 1.json中的参数可以使用param参数传入
+NiceFlow exec --path 1.json --param '{"name":"test"}'
+```
+
+##### sql模式执行flow任务
+
+```shell
+# --sql_script,"sql脚本语句，支持多行"
+# --sql_path,"sql脚本文件，支持多行,和sql_script二选一"
+# --db_path, "输入duckdb数据库的路径，不存在则为内存模式[可选]"
+# --res_path,"输入文件路径，该路径下的文件会被自动加载到db中[可选]"
+# --function_path,"输入函数路径，该路径为python文件,可以作为数据库自定义函数使用[可选]"
+NiceFlow sql --sql_path 1.sql \   
+--res_path='C:/Users/xiaow/Desktop/22/test' \
+--function_path='C:/Users/xiaow/Desktop/22/test/1.python' 
+
+# sql语句
+copy  (select f_print(d_date) from msd_2024 where d_date = '2023-12-31') to  'C:/Users/xiaow/Desktop/22/test/2.csv';
+select f_print(d_date) from msd_2024 where d_date = '2023-12-31';
+
+
+# python文件中定义函数
+def f_print(x:str)->str:
+    return x+"___";
+
+```
 
 #### 代码使用
 
@@ -45,18 +70,32 @@ NiceFlow exec --path 1.json
     "name": "",
     "uid": "",
     "param": {
-
-    } },
+    }
+  },
   "nodes": [
     {
       "id": "FakerInput",
       "name": "read1",
       "type": "input",
       "properties": {
-        "rows":10000,
-        "columns": ["name","address","city","street_address","date_of_birth","phone_number"],
-        "randoms":[
-          {"key":"sex","values":["男","女","未知"]}
+        "rows": 10000,
+        "columns": [
+          "name",
+          "address",
+          "city",
+          "street_address",
+          "date_of_birth",
+          "phone_number"
+        ],
+        "randoms": [
+          {
+            "key": "sex",
+            "values": [
+              "男",
+              "女",
+              "未知"
+            ]
+          }
         ]
       }
     },
@@ -79,11 +118,11 @@ NiceFlow exec --path 1.json
 
 ```
 
-
 ```python
 import os
 from NiceFlow.core.flow import Flow
 from NiceFlow.core.manager import FlowManager
+
 
 def getProjectPath() -> str:
     # 获取当前文件的绝对路径
@@ -106,14 +145,9 @@ if __name__ == '__main__':
 
 ```
 
-
 ### 架构图
 
-
-
 ### 插件使用说明文档
-
-
 
 #### 输入
 
@@ -132,7 +166,6 @@ if __name__ == '__main__':
 | ESInput         | 读取Elasticsearch数据 | 完成   | [Elasticsearch输入](doc/doc/ESInput.md) |         
 | MongoDBInput    | 读取MongoDB数据       | 完成   | [MongoDB输入](doc/doc/MongoDBInput.md) |
 | MqttInput       | 从Mqtt Broker读取数据  |    | [MqttInput输入](doc/doc/MqttInput.md) |
-
 
 #### 转换
 
@@ -158,8 +191,8 @@ if __name__ == '__main__':
 | Duplicate      | 去重器  | 完成   | [去重器](doc/doc/Duplicate.md) |
 | Console        | 控制台打印 | 完成   | [控制台输出](doc/doc/Console.md)    |
 
-
 #### 输出
+
 | 插件              | 功能           | 完成情况 | 文档                            |
 |-----------------|------------|------|-------------------------------|
 | FileOutput      | 文件输出           | 完成   | [文件输出](doc/doc/FileOutput.md) |
@@ -186,7 +219,6 @@ if __name__ == '__main__':
 | CsvOutput       | CSV输出           | 完成   | [CSV输出](doc/doc/CsvOutput.md) |
 | CosOutput       | COS输出           |      |      |
 | ClickHouseOutput | ClickHouse输出    |      |      |
-
 
 ### 数据实战
 
--- src/NiceFlow/cli/cli.py
+++ src/NiceFlow/cli/cli.py
@@ -38,10 +38,13 @@ def explore():
 
 @cli.command('sql', short_help='sql a data ')
 @click.option("--sql_script", default="", help="sql脚本语句，支持多行")
+@click.option("--sql_path", default="", help="sql脚本文件")
 @click.option("--db_path", default="", help="输入duckdb数据库的路径，不存在则为内存模式[可选]")
 @click.option("--res_path", default="", help="输入文件路径，该路径下的文件会被自动加载到db中[可选]")
 @click.option("--function_path", default="", help="输入函数路径，该路径为python文件,可以作为数据库自定义函数使用[可选]")
-def sql(sql_script: str, db_path: str = None, res_path: str = None, function_path: str = None):
+@click.option("--result_path", default="", help="输入结果路径,支持csv,json,parquet,txt,excel,clipboard当输入为clipboard时,结果直接写入剪贴板")
+def sql(sql_script: str = None, sql_path: str = None, db_path: str = None, res_path: str = None,
+        function_path: str = None, result_path: str = None):
     if db_path:
         con = duckdb.connect(db_path)
     else:
@@ -49,32 +52,62 @@ def sql(sql_script: str, db_path: str = None, res_path: str = None, function_pat
     # 自动加载目录下的res文件
     if res_path:
         for file in os.listdir(res_path):
-            table_path = res_path+"/"+file
+            table_path = res_path + "/" + file
+            if len(file.split(".")) <= 1:
+                continue
             table_name = file.split(".")[0]
-            if file.endswith(".csv"):
+            file_suffix = file.split(".")[1]
+            if file_suffix.lower() == "csv":
                 con.sql(f'''
                 drop table if EXISTS  {table_name};
                 CREATE TABLE {table_name} AS SELECT * FROM read_csv_auto('{table_path}',header = true);
                 ''')
-            if file.endswith(".parquet"):
+            if file_suffix.lower() == "parquet":
                 con.sql(f'''
                 drop table if EXISTS  {table_name};
                 CREATE TABLE {table_name} AS SELECT * FROM read_parquet('{table_path}');
                 ''')
-            if file.endswith(".xlsx"):
+            if file_suffix.lower() == "xlsx":
                 con.sql(f'''
+                INSTALL spatial;
+                LOAD spatial;
                 drop table if EXISTS  {table_name};
                 CREATE TABLE {table_name} AS SELECT * FROM st_read('{table_path}');
+                ''')
+            if file_suffix.lower() == "json":
+                con.sql(f'''
+                drop table if EXISTS  {table_name};
+                CREATE TABLE {table_name} AS SELECT * FROM read_json('{table_path}',auto_detect=true);
                 ''')
     if function_path:
         module = load_module(function_path)
         items = inspect.getmembers(module, inspect.isfunction)
         for item in items:
-            con.create_function(item[0],item[1])
-    duck_df = con.sql(sql_script)
-    df =  duck_df.to_df()
+            con.create_function(item[0], item[1])
+    if sql_script:
+        duck_df = con.sql(sql_script)
+    else:
+        with open(sql_path, "r", encoding='utf8') as f:
+            duck_df = con.sql(f.read())
+
+    if duck_df is not None:
+        duck_df.show(max_width=1000, max_rows=100)
+        df = duck_df.to_df()
+        logger.info(f"查询结果总数为：{len(df)}")
+        if result_path:
+            result_path = result_path.strip()
+            if result_path.endswith(".csv"):
+                df.to_csv(result_path, index=False)
+            elif result_path.endswith(".xlsx"):
+                df.to_excel(result_path, index=False)
+            elif result_path.endswith(".json"):
+                df.to_json(result_path, index=False)
+            elif result_path.endswith(".parquet"):
+                df.to_parquet(result_path, index=False)
+            elif result_path.endswith("clipboard"):
+                logger.info(f"文件已经写入剪贴板")
+                df.to_clipboard()
     con.close()
-    return df
 
 
 @cli.command('chart', short_help='chart a data ')
@@ -131,10 +164,11 @@ def decrypt(path: str, password: str):
 
 if __name__ == '__main__':
     sql_script = '''
-    copy  (select f_print(d_date) from msd_2024 where d_date = '2023-12-31') to  'C:/Users/xiaow/Desktop/22/test/2.csv';
-    select f_print(d_date) from msd_2024 where d_date = '2023-12-31';
+    select * from total_RQ_DEVICE_20240130;
     '''
-    duck_df = sql(sql_script,db_path=None,
+    duck_df = sql(sql_script, db_path=None,
                   res_path="C:/Users/xiaow/Desktop/22/test",
-                  function_path="C:/Users/xiaow/Desktop/22/test/common_function.py")
-    print(duck_df)
+                  function_path="C:/Users/xiaow/Desktop/22/test/common_function.py",
+                  # result_path="C:/Users/xiaow/Desktop/22/test/result.csv"
+                  # result_path="C:/Users/xiaow/Desktop/22/test/result.csv"
+                  )
